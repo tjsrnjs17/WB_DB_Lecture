e26f5668e1127a643f7d4a1418f7300d7cee6887
dnjswns0 <69837695+dnjswns0@users.noreply.github.com>
2020-08-18 15:40:02 +0900

    Update loginCheck.php

--- SourceCode/2일차(깃헙)/loginCheck.php
+++ SourceCode/2일차(깃헙)/loginCheck.php
@@ -1,24 +1,24 @@
 ﻿
 <?php
-	$con = mysqli_connect("localhost", "root", "", "setest");
+	$con = mysqli_connect("localhost", "root", "", "setest"); //db연결
 
-if($_POST['user_id'] && $_POST['user_pw']){
-	$resource = mysqli_query( $con,"SELECT * FROM user WHERE id = '".$_POST['user_id']."' and pw = '".$_POST['user_pw']."'" );
-	$num = mysqli_num_rows( $resource );
+if($_POST['user_id'] && $_POST['user_pw']){ //양식으로 전송된 데이터 받기
+	$resource = mysqli_query( $con,"SELECT * FROM user WHERE id = '".$_POST['user_id']."' and pw = '".$_POST['user_pw']."'" );//쿼리문 전송, REST api 사용하기 
+	$num = mysqli_num_rows( $resource ); //문자열 숫자로 변환
 	if($num>0) {
-		setcookie('user_id',$_POST['user_id'],time()+(86400*30),'/');
-		setcookie('user_pw',$_POST['user_pw'],time()+(86400*30),'/');
-		echo "<meta http-equiv='refresh' content='0;url=main.html'>";
+		setcookie('user_id',$_POST['user_id'],time()+(86400*30),'/'); //아이디 쿠키 저장
+		setcookie('user_pw',$_POST['user_pw'],time()+(86400*30),'/'); //비밀번호 쿠키 저장
+		echo "<meta http-equiv='refresh' content='0;url=main.html'>"; // 페이지 메인으로 넘기기
 		}
 	else{
-		echo "<script>alert('아이디 또는 패스워드가 잘못되었습니다.');history.back();</script>";
+		echo "<script>alert('아이디 또는 패스워드가 잘못되었습니다.');history.back();</script>"; //문자열 출력, 이전페이지로 돌리기
 		exit;
 	}	
 
 }
 else{
 
-echo "<script>alert('아이디 또는 패스워드를 입력하세요.');history.back();</script>";
+echo "<script>alert('아이디 또는 패스워드를 입력하세요.');history.back();</script>"; // 경고창 띄우기, 이전페이지로 돌아가기
 	exit;
 }
 ?>
