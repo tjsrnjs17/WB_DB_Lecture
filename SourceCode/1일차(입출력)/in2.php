c7018ab98516196c2002edf2ba00c03b2e791994
NamJaeHyeon <54064126+NamJaeHyeon@users.noreply.github.com>
2020-08-18 15:56:56 +0900

    Update in2.php

--- SourceCode/1일차(입출력)/in2.php
+++ SourceCode/1일차(입출력)/in2.php
@@ -1,16 +1,16 @@
-<html>
-<body>
-<form action = "in2.php" method= "post">
-input: <input type = "text" name="content"/>
-input2: <input type = "text" name="content2"/>
-<input type = "submit"/>
-</form>
-</body>
-</html>
+<html> // html 태그 시작
+<body> // body 태그 시작
+<form action = "in2.php" method= "post"> // form 태그 시작
+input: <input type = "text" name="content"/> // 문서 내용 "input:"과 "content라는 이름의 속성을 가진 input 태그
+input2: <input type = "text" name="content2"/> // 문서 내ㅑ용 "input2:"과 content2라는 이름의 속성을 가진 input 태그
+<input type = "submit"/> // submit 타입의 속성을 가진 input 태그
+</form> // form 태그 닫기
+</body> // body 태그 닫기
+</html> // html 태그 닫기
 
-<?php
-	$con = mysqli_connect("localhost", "root", "", "io");
-	$sql = "INSERT INTO testd (content,content2) VALUES('$_POST[content]','$_POST[content2]')";
-	$result = mysqli_query( $con, $sql );
+<?php // php 언어 시작
+	$con = mysqli_connect("localhost", "root", "", "io"); // sql에 연결
+	$sql = "INSERT INTO testd (content,content2) VALUES('$_POST[content]','$_POST[content2]')"; // 클라이언트로부터 받은 변수를 sql에 생성하기
+	$result = mysqli_query( $con, $sql ); // 결과를 con과 sql을 파라미터로 실행시킨 mysql_query의 함숫값으로 정하기
 	
-?>
+?> // php 언어 끝
