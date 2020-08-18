b6f82e11fbe51d581ffa3840a9b558d18be4e346
Re2der <69837550+Re2der@users.noreply.github.com>
2020-08-18 16:20:11 +0900

    Update in2.php

--- SourceCode/1일차(입출력)/in2.php
+++ SourceCode/1일차(입출력)/in2.php
@@ -1,16 +1,17 @@
 <html>
 <body>
-<form action = "in2.php" method= "post">
-input: <input type = "text" name="content"/>
-input2: <input type = "text" name="content2"/>
-<input type = "submit"/>
+<form action = "in2.php" method= "post">//버튼 눌리면 action = in.php 재귀 함수 처럼 자기 자신 부름 
+input: <input type = "text" name="content"/>//입력받은 text값을 content의 넣는다
+input2: <input type = "text" name="content2"/>입력받은 text값을 content2의 넣는다
+<input type = "submit"/>//제출버튼
 </form>
 </body>
 </html>
 
 <?php
-	$con = mysqli_connect("localhost", "root", "", "io");
-	$sql = "INSERT INTO testd (content,content2) VALUES('$_POST[content]','$_POST[content2]')";
-	$result = mysqli_query( $con, $sql );
+	$con = mysqli_connect("localhost", "root", "", "io");//db을 불러옴
+	$sql = "INSERT INTO testd (content,content2) VALUES('$_POST[content]','$_POST[content2]')";////칼럼 content에 '$_POST[content]에 저장된 값 삽입 시키는 쿼리문
+	$result = mysqli_query( $con, $sql );//쿼리문 전송
+
 	
 ?>
