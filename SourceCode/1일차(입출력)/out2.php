227b4cdec0cf2672ed614274b6b8f747648dfd3a
Park Ji Eun <69837552+j1eunn0510@users.noreply.github.com>
2020-08-18 16:05:42 +0900

    Update out2.php

--- SourceCode/1일차(입출력)/out2.php
+++ SourceCode/1일차(입출력)/out2.php
@@ -1,12 +1,13 @@
 <?php
-	$con = mysqli_connect("localhost", "root", "", "io");
-	$sql = "SELECT * FROM testd";
-	$result = mysqli_query( $con, $sql );
-	while($row = mysqli_fetch_array($result)){
-		echo '<p>'; 
-		echo $row['content'];
-		echo '<br>';
-		echo $row['content2'];
+	$con = mysqli_connect("localhost", "root", "", "io");//DB연결
+	$sql = "SELECT * FROM testd";//쿼리문
+	$result = mysqli_query( $con, $sql );//쿼리문 전송
+	while($row = mysqli_fetch_array($result)){//쿼리 실행으로 얻은 set 에서 레코드 1개씩 리턴
+
+		echo '<p>'; //문자열출력
+		echo $row['content'];//POST방식으로 저장
+		echo '<br>';//문자열출력
+		echo $row['content2'];//POST방식으로 저장
 		}
 ?>
 
