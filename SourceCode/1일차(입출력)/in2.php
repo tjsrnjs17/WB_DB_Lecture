1651cbd2b4d6423e240f26ce2543e06997dcf42a
Im-ChangSun <69837572+Im-ChangSun@users.noreply.github.com>
2020-08-18 16:16:45 +0900

    Update in2.php

--- SourceCode/1일차(입출력)/in2.php
+++ SourceCode/1일차(입출력)/in2.php
@@ -1,16 +1,16 @@
 <html>
 <body>
-<form action = "in2.php" method= "post">
-input: <input type = "text" name="content"/>
-input2: <input type = "text" name="content2"/>
-<input type = "submit"/>
+<form action = "in2.php" method= "post"> //in2.php파일을 불러옴
+input: <input type = "text" name="content"/> //입력 받을 수 있는 창을 만듦
+input2: <input type = "text" name="content2"/> //입력 받을 수 있는 창을 만듦
+<input type = "submit"/>//제출 버튼
 </form>
 </body>
 </html>
 
 <?php
-	$con = mysqli_connect("localhost", "root", "", "io");
-	$sql = "INSERT INTO testd (content,content2) VALUES('$_POST[content]','$_POST[content2]')";
-	$result = mysqli_query( $con, $sql );
+	$con = mysqli_connect("localhost", "root", "", "io"); //db불러오기
+	$sql = "INSERT INTO testd (content,content2) VALUES('$_POST[content]','$_POST[content2]')"; //testd라는 칼럼에 입력받은 데이터를 삽입시키는 쿼리문 
+	$result = mysqli_query( $con, $sql ); //쿼리문 전송
 	
 ?>
