155a3f3aa7349bc101ec22574197e95600b9a46a
Park Ji Eun <69837552+j1eunn0510@users.noreply.github.com>
2020-08-18 15:56:35 +0900

    Update in2.php

--- SourceCode/1일차(입출력)/in2.php
+++ SourceCode/1일차(입출력)/in2.php
@@ -1,16 +1,16 @@
 <html>
 <body>
-<form action = "in2.php" method= "post">
-input: <input type = "text" name="content"/>
-input2: <input type = "text" name="content2"/>
-<input type = "submit"/>
+<form action = "in2.php" method= "post">//post형식으로 값 넘김
+input: <input type = "text" name="content"/>//텍스트박스
+input2: <input type = "text" name="content2"/>//텍스트박스
+<input type = "submit"/>//제출버튼
 </form>
 </body>
 </html>
 
 <?php
-	$con = mysqli_connect("localhost", "root", "", "io");
-	$sql = "INSERT INTO testd (content,content2) VALUES('$_POST[content]','$_POST[content2]')";
-	$result = mysqli_query( $con, $sql );
+	$con = mysqli_connect("localhost", "root", "", "io");//DB연결
+	$sql = "INSERT INTO testd (content,content2) VALUES('$_POST[content]','$_POST[content2]')";//쿼리문
+	$result = mysqli_query( $con, $sql );//쿼리문전송
 	
 ?>
