639be6a546c5efaf2c5f117edd608f050d59da7f
khs442 <69837529+khs442@users.noreply.github.com>
2020-08-18 15:45:42 +0900

    Update out2.php

--- SourceCode/1일차(입출력)/out2.php
+++ SourceCode/1일차(입출력)/out2.php
@@ -1,13 +1,13 @@
 <?php
-	$con = mysqli_connect("localhost", "root", "", "io");
-	$sql = "SELECT * FROM testd";
-	$result = mysqli_query( $con, $sql );
+	$con = mysqli_connect("localhost", "root", "", "io"); //DB연결
+	$sql = "SELECT * FROM testd"; //쿼리문 요청
+	$result = mysqli_query( $con, $sql ); //쿼리문 전송
 	while($row = mysqli_fetch_array($result)){
-		echo '<p>'; 
-		echo $row['content'];
-		echo '<br>';
+		echo '<p>'; //문단 띄우기
+		echo $row['content'];  
+		echo '<br>'; //줄 
 		echo $row['content2'];
-		}
+		} //$row 출력 
 ?>
 
 
