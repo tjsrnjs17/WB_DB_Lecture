a468d0a8c13dc8dce795eff892aed49088271fdf
Jeongmin-221 <69837697+Jeongmin-221@users.noreply.github.com>
2020-08-18 16:27:18 +0900

    Update out2.php

--- SourceCode/1일차(입출력)/out2.php
+++ SourceCode/1일차(입출력)/out2.php
@@ -1,12 +1,14 @@
 <?php
-	$con = mysqli_connect("localhost", "root", "", "io");
+	$con = mysqli_connect("localhost", "root", "", "io");//db연결
 	$sql = "SELECT * FROM testd";
-	$result = mysqli_query( $con, $sql );
+	$result = mysqli_query( $con, $sql );//쿼리문 전송
+
 	while($row = mysqli_fetch_array($result)){
 		echo '<p>'; 
-		echo $row['content'];
+		echo $row['content'];//$row['content']에 전에 입력한 값이 들어가있다 a를 입력하고 제출을 누르면 POST방식으로 in.php로 넘어가 $row['content’]에 저장되있다
 		echo '<br>';
-		echo $row['content2'];
+		echo $row['content2'];//$row['content2']에 전에 입력한 값이 들어가있다 a를 입력하고 제출을 누르면 POST방식으로 in.php로 넘어가 $row['content2’]에 저장되있다
+
 		}
 ?>
 
